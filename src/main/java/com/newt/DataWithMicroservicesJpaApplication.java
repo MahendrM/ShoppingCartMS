b2aec22c58d7cbb831a823fbb6bb5939d4485b34
Lokesh@Newt <lokeshkumarm@newtglobal.com>
2016-10-25 16:36:24 +0530

    Update DataWithMicroservicesJpaApplication.java

--- src/main/java/com/newt/DataWithMicroservicesJpaApplication.java
+++ src/main/java/com/newt/DataWithMicroservicesJpaApplication.java
@@ -5,15 +5,17 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.ConfigurableApplicationContext;
+
+import com.newt.shoppingcart.controller.KafkaController;
+
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import com.newt.shoppingcart.controller.KafkaController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -38,7 +40,8 @@ public class DataWithMicroservicesJpaApplication {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.newt.shoppingcart.controller")).build().apiInfo(apiInfo());
+				.apis(RequestHandlerSelectors.basePackage("com.newt.shoppingcart.controller")).build()
+				.apiInfo(apiInfo());
 	}
 
 	private ApiInfo apiInfo() {
