6ff45853e6fd0fdb650d544c202c237ffc923a42
Lokesh@Newt <lokeshkumarm@newtglobal.com>
2016-10-25 16:34:12 +0530

    Update DataWithMicroservicesJpaApplication.java

--- src/main/java/com/newt/DataWithMicroservicesJpaApplication.java
+++ src/main/java/com/newt/DataWithMicroservicesJpaApplication.java
@@ -13,6 +13,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import com.newt.shoppingcart.controller.KafkaController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
